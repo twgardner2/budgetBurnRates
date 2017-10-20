d9716ae3f24509d6654952ea3629e8e15062eebe
Tom Gardner <twgardner2@gmail.com>
2017-10-20 08:27:30 +0200

    add FY and FQ calculations to data

--- server.R
+++ server.R
@@ -8,26 +8,45 @@ library(shiny)
 
 remove(list = ls())
 
+### Define functions ####
+dateToFY   <- function(x) {ifelse(lubridate::month(x) %in% c(10, 11, 12), lubridate::year(x) + 1, lubridate::year(x))}
+# dateToFQ   <- function(x) {
+#                   switch(lubridate::quarter(x),
+#                     2,
+#                     3,
+#                     4,
+#                     1) 
+#                   }
+dateToFQ <- function(x) {
+  y <- lubridate::quarter(x)
+  z <- ifelse(y==1, 2, ifelse(y==2, 3, ifelse(y==3, 4, 1)))
+  return(z)
+}
+
+###
+
 
 data_file <- "NWA_Directorates_Obligations_FY17.xlsx"
 #data_file <- "//10.6.100.235/tacnasa1$/Public/J8/Gardner/FY18 Budget Forecasting/app/NWA_Directorates_Obligations_FY17.xlsx"
-
-#path <- 
 #data_file <- "//10.6.100.235/tacnasa1$/Public/J8/Gardner/FY18 Budget Forecasting/app/data.xlsx"
 
+### Read in data
 expData <- read_xlsx(data_file, sheet = 1, trim_ws = TRUE)
 wbsList <- read_xlsx(data_file, sheet = 2, trim_ws = TRUE)
 
-#colnames(expData) <- tolower(colnames(expData))
-colnames(expData) <- c("wbs", "refDoc", "commitmentItem", "commitmentItemCat", "postDate", "dateUpdated", "obligation")
-
-
+### Clean column names
+colnames(expData) <- c("wbs", "refDoc", "commitmentItem", "commitmentItemCat", "postDate", "updatedDate", "obligation")
 colnames(wbsList) <- tolower(colnames(wbsList))
 
+### Clean data types
+expData$postDate    <- lubridate::as_date(expData$postDate)
+expData$updatedDate <- lubridate::as_date(expData$updatedDate)
+
+### Join wbs with directorate, calculate additional columns
 expData <- dplyr::left_join(expData, wbsList, by = c("wbs" = "wbs"))
 expData <- expData %>% filter(commitmentItem != "Result")
-
-
+expData <- expData %>% mutate(fiscalYear = dateToFY(postDate),
+                              fiscalQtr  = dateToFQ(postDate))
 
 ### Table of expenses by Quarter
 expDataByQtr <- expData %>% mutate(period = lubridate::quarter(.$postDate, with_year = TRUE)) %>% 
